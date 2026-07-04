bf431925bfbdcdbcae480414242324711ea957d6
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2026-07-04 11:53:56 +0200

    doc: doc for scala test

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,5 +3,7 @@ import sbt.*
 object Dependencies:
   object Versions:
     val scalaTest = "3.2.20"
-
-  val scalaTest = "org.scalatest" %% "scalatest" % Versions.scalaTest
+  /**
+   * ScalaTes.
+   */
+  val scalaTest: ModuleID = "org.scalatest" %% "scalatest" % Versions.scalaTest
