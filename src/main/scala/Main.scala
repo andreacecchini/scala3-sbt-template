cf9a45179e592d70fc84eb3261cb4336c849dcdf
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2026-07-04 11:10:33 +0200

    style: format

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,2 +1,3 @@
 object Main:
-  def main(args: Array[String]): Unit = println("Hello from scala3-sbt-template!")
+  def main(args: Array[String]): Unit = println:
+    "Hello from scala3-sbt-template!"
