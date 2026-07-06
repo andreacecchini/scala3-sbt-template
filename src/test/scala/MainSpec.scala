04868f23f3141e4d7af948c0a42aecfaa46946e2
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2026-07-06 11:21:54 +0200

    fix: check format on tests

--- src/test/scala/MainSpec.scala
+++ src/test/scala/MainSpec.scala
@@ -2,5 +2,5 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class MainSpec extends AnyFlatSpec with Matchers:
-  "truth" should "hold" in :
+  "truth" should "hold" in:
     true shouldBe true
